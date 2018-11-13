604b5d84e95b31e3209fdc1e09edf3d056456fa2
NadaBasha <Nada.basha@itworx.com>
2018-11-13 23:38:42 +0200

    Adding authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
+import { Security, ImplicitCallback } from '@okta/okta-react';
 import NavBar from './components/navBar';
 import Shapes from './components/shapes';
 import Users from './components/users';
 import NotFound from './components/notFound';
+import Home from './components/home';
+
+const config = {
+  issuer: 'https://dev-387262.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oahgxzktd6OPlsYW0h7'
+}
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <main className="container">
-          <Switch>
-            <Route path="/users" component={Users} />
-            <Route path="/shapes" component={Shapes} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" exact to="/users" />
-            <Redirect to="/not-found" />
-          </Switch>
-        </main>
+        <Security issuer={config.issuer} client_id={config.client_id} redirect_uri={config.redirect_uri}>
+          <NavBar />
+          <main className="container">
+            <Switch>
+              <Route path="/users" component={Users} />
+              <Route path="/shapes" component={Shapes} />
+              <Route path="/not-found" component={NotFound} />
+              <Route path="/" exact component={Home} />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+              <Redirect to="/not-found" />
+            </Switch>
+          </main>
+        </Security>
       </React.Fragment>
     );
   }
